f6cd09a3fef95e2af4ed1ab6ae3b205f561ade98
turjoy-real <sahaturjoy@gmail.com>
2025-05-26 02:51:28 +0530

    RBAC implemented in productservice

---
+++ productservice/src/main/java/com/services/productservice/configs/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.services.productservice.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // .securityMatcher("/api/**")
+                // .csrf(csrf -> csrf.disable())
+                // .authorizeHttpRequests(authorize -> authorize
+                // .requestMatchers("/api/products/**")
+                // .permitAll()
+                // .anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/api/products").hasRole("USER")
+                        // .permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .cors(withDefaults())
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // 👈 matches your token claim
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // 👈 required prefix
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+
+        return authenticationConverter;
+    }
+
+}
--- productservice/src/main/java/com/services/productservice/controllers/ProductController.java
+++ productservice/src/main/java/com/services/productservice/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
