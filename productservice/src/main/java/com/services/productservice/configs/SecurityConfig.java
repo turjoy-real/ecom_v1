52055b65a12c5f10a263407917f035dc653e4c8b
turjoy-real <sahaturjoy@gmail.com>
2025-07-04 17:14:15 +0530

    product test mocks setup

--- productservice/src/main/java/com/services/productservice/configs/SecurityConfig.java
+++ productservice/src/main/java/com/services/productservice/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.services.productservice.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Profile("!test")
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
--- productservice/src/test/java/com/services/productservice/ProductserviceApplicationTests.java
+++ productservice/src/test/java/com/services/productservice/ProductserviceApplicationTests.java
@@ -1,10 +1,37 @@
 package com.services.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@SuppressWarnings("removal")
 class ProductserviceApplicationTests {
+
+    @Autowired
+	private MockMvc mockMvc;
+    
+    @MockBean
+    private com.services.productservice.repositories.ProductRepository productRepository;
+
+    @MockBean
+    private com.services.productservice.repositories.ProductElasticsearchRepository productElasticsearchRepository;
+
+    @MockBean
+    private com.services.productservice.repositories.CategoryRepository categoryRepository;
+
+    @MockBean
+    private org.springframework.data.redis.core.RedisTemplate<Long, Object> redisTemplate;
+
+    @MockBean
+    private com.services.productservice.services.CacheService cacheService;
+
+    @MockBean
+    private org.springframework.security.oauth2.jwt.JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
